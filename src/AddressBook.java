121a379faab0b9195b2060fe50023d476fcbb131
Tomisin Ajayi <ajayitomisin50@gmail.com>
2022-09-19 15:56:39 -0400

    Changed the name.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -16,7 +16,7 @@ public class AddressBook {
     }
 
     public static void main(String[] args){
-        BuddyInfo buddy = new BuddyInfo("Tom", "Revalie", "6132128849");
+        BuddyInfo buddy = new BuddyInfo("Adam", "Revalie", "6132128849");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
