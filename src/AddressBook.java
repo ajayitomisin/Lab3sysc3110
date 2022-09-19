b426a8aa71c46e95f38d61b5bbd3756f5443d50e
Tomisin Ajayi <ajayitomisin50@gmail.com>
2022-09-19 15:38:57 -0400

    Added some function calls to test Git commit.

---
+++ src/AddressBook.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddies;
+
+    public AddressBook() {
+        buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo newBuddy){
+        buddies.add(newBuddy);
+    }
+
+    public void removeBuddy(BuddyInfo newBuddy){
+        buddies.remove(newBuddy);
+    }
+
+    public static void main(String[] args){
+        BuddyInfo buddy = new BuddyInfo("Tom", "Envie", "6132128849");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+}
