604cc3c8eaea5a049a099d7785e948338b7b7f47
ajayitomisin <82481467+ajayitomisin@users.noreply.github.com>
2022-09-26 17:07:23 -0400

    Added a print statement.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,5 +20,6 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("Done!");
     }
 }
